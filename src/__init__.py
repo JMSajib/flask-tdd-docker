a7b6e6ad81eb0399b377147a0841667e5214d4f0
JMSajib <jm.sajib012@gmail.com>
2022-02-23 23:48:43 +0600

    Added postgres database service

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 
@@ -8,8 +10,25 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
-app.config.from_object('src.config.DevelopmentConfig')
+# DB instantiate
+
+db = SQLAlchemy(app)
+
+# Model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self,username,email):
+        self.email = email
+        self.username = username
+
 
 class Ping(Resource):
     def get(self):
--- src/config.py
+++ src/config.py
@@ -1,17 +1,18 @@
-from unittest.mock import Base
+import os
 
 
 class BaseConfig:
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class ProductionConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
