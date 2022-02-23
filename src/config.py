282c962d17c9ea94a4ecbd5b2c1b8670a3cfcf42
JMSajib <jm.sajib012@gmail.com>
2022-02-23 15:42:31 +0600

    initial commit

---
+++ src/config.py
@@ -0,0 +1,17 @@
+from unittest.mock import Base
+
+
+class BaseConfig:
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    pass
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    pass
