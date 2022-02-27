e5fb6441a554da5bc43b964cfcfae5131302eb62
JMSajib <jm.sajib012@gmail.com>
2022-02-27 17:37:23 +0600

    Resolved docker issue in mac silicon

--- src/__init__.py
+++ src/__init__.py
@@ -3,38 +3,22 @@ from flask import Flask, jsonify
 from flask_restx import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
-# instantiate the app
-
-app = Flask(__name__)
-
-api = Api(app)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
 # DB instantiate
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# Model
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
+    app = Flask(__name__)
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-    def __init__(self,username,email):
-        self.email = email
-        self.username = username
+    db.init_app(app)
 
+    from src.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
 
-class Ping(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'Pong!!!!'
-        }
-
-api.add_resource(Ping, '/ping')
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+    return app
